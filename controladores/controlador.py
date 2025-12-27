faa45b94db4788f65f70af9433b243e0cb0204f5
Cristian Gonzalez <cs.gonzalez3@gmail.com>
2025-12-26 22:24:32 -0300

    feat: crea controlador.py para manejar logica

---
+++ controladores/controlador.py
@@ -0,0 +1,20 @@
+from modelos.tarea import Tarea
+from vistas.ventana import VentanaTareas
+
+class ControladorTareas:
+    def __init__(self):
+        self.tareas = []
+        self.ventana = VentanaTareas(self)
+
+    def agregar_tarea(self, titulo):
+        nueva_tarea = Tarea(titulo)
+        self.tareas.append(nueva_tarea)
+        self.ventana.mostrar_tareas(self.tareas)
+
+    def completar_tarea(self, indice):
+        if 0 <= indice < len(self.tareas):
+            self.tareas[indice].marcar_completada()
+            self.ventana.mostrar_tareas(self.tareas)
+
+    def iniciar(self):
+        self.ventana.iniciar()
